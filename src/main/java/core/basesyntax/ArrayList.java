44571c03ffa6899afbfc63fcbc0fd5a6dd10cce4
Andrii <andriisopovskyi@gmail.com>
2026-06-14 00:04:04 +0200

    finish

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -1,48 +1,105 @@
 package core.basesyntax;
 
+import java.util.NoSuchElementException;
+
 public class ArrayList<T> implements List<T> {
+    private Object[] elements;
+    private int size;
+
     @Override
     public void add(T value) {
-
+        if (size == elements.length) {
+            Object[] newArray = new Object[(int) (elements.length * 1.5)];
+            for (int i = 0; i < elements.length; i++) {
+                newArray[i] = elements[i];
+            }
+            elements = newArray;
+        }
+        elements[size] = value;
+        size++;
     }
 
     @Override
     public void add(T value, int index) {
-
+        if (index < 0 || index > size) {
+            throw new ArrayListIndexOutOfBoundsException("Error");
+        }
+        if (size == elements.length) {
+            Object[] newArray = new Object[(int) (elements.length * 1.5)];
+            for (int i = 0; i < elements.length; i++) {
+                newArray[i] = elements[i];
+            }
+            elements = newArray;
+        }
+        for (int i = size; i > index; i--) {
+            elements[i] = elements[i - 1];
+        }
+        elements[index] = value;
+        size++;
     }
 
     @Override
     public void addAll(List<T> list) {
-
+        for (int i = 0; i < list.size(); i++) {
+            add(list.get(i));
+        }
     }
 
     @Override
     public T get(int index) {
-        return null;
+        if (index < 0 || index >= size) {
+            throw new ArrayListIndexOutOfBoundsException("Error");
+        }
+        return (T) elements[index];
     }
 
     @Override
-    public void set(T value, int index) {
-
+    public T set(T value, int index) {
+        if (index < 0 || index >= size) {
+            throw new ArrayListIndexOutOfBoundsException("Error");
+        }
+        T oldValue = (T) elements[index];
+        elements[index] = value;
+        return oldValue;
     }
 
     @Override
     public T remove(int index) {
-        return null;
+        if (index < 0 || index >= size) {
+            throw new ArrayListIndexOutOfBoundsException("Error");
+        }
+        T oldValue = (T) elements[index];
+        for (int i = index; i < size; i++) {
+            elements[i] = elements[i + 1];
+        }
+        elements[size - 1] = null;
+        size--;
+        return oldValue;
     }
 
     @Override
     public T remove(T element) {
-        return null;
+        for (int i = 0; i < size; i++) {
+            if (elements[i].equals(element)) {
+                T oldValue = (T) elements[i];
+                for (int k = i; k < size - 1; k++) {
+                    elements[k] = elements[k + 1];
+                }
+                elements[size - 1] = null;
+                size--;
+                return (T) oldValue;
+            }
+        }
+        throw new NoSuchElementException("Error");
     }
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size == 0;
     }
 }
